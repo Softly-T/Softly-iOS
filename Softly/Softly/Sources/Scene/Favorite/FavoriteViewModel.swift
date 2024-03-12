2499e37d21dc01b6e138589821a85697700b6c27
가현 <102145605+rkgus24@users.noreply.github.com>
2024-03-12 11:25:20 +0900

    Delete Softly directory

--- Softly/Softly/Sources/Scene/Favorite/FavoriteViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FavoriteViewModel.swift
-//  Softly
-//
-//  Created by 노가현 on 3/12/24.
-//
-
-import Foundation
--- Softly/Softly/Sources/Scene/Profile/ProfileViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileViewModel.swift
-//  Softly
-//
-//  Created by 노가현 on 3/12/24.
-//
-
-import Foundation
