38dec2ecdaa89075d5b140fb98fe118f51d0585b
가현 <102145605+rkgus24@users.noreply.github.com>
2024-03-20 22:01:25 +0900

    ✨ :: HomeView 퍼블리싱 + MainTab 하단바 구현

---
+++ Softly/Softly/Sources/DesignService/Arrow/ArrowView.swift
@@ -0,0 +1,18 @@
+//
+//  ArrowView.swift
+//  Softly
+//
+//  Created by 노가현 on 3/20/24.
+//
+
+import SwiftUI
+
+struct ArrowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArrowView()
+}
---
+++ Softly/Softly/Sources/DesignService/Tab/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Softly
+//
+//  Created by 노가현 on 3/20/24.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
